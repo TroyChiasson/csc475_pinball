174fddf0f75a84d22440b6b43f450735d365332e
ctm042 <ctm042@latech.edu>
2024-03-23 22:03:09 -0500

    added light fade outs to bumper collisions.

--- csc475_pinball/Assets/Script/Bumper.cs
+++ csc475_pinball/Assets/Script/Bumper.cs
@@ -1,14 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Bumper : MonoBehaviour {
     public float force;
+    //private float light = GetComponent<Light>();
+    private Light bumperLight;
+    public float fadeDuration = 1f; // Duration in seconds to fade out
+    private float initialIntensity;
+
+    void Start()
+    {
+        // Get bumper's light component
+        bumperLight = GetComponent<Light>();
+        // Store the initial intensity of the light
+        initialIntensity = bumperLight.intensity;
+        // Turn off light
+        bumperLight.intensity = 0f;
+    }
 
     void OnCollisionEnter(Collision col) {
-        Debug.Log("Collision!");
         if(col.gameObject.tag == "Ball") {
-            Debug.Log("With Ball!");
             // Get the rigid body of the collided ball
             Rigidbody ballrb = col.gameObject.GetComponent<Rigidbody>();
             // Calculate angle between the collision point and the ball
@@ -17,6 +30,44 @@ public class Bumper : MonoBehaviour {
             dir = -dir.normalized;
             // Add force in the direction of dir and multiply it by force
             ballrb.AddForce(dir * force, ForceMode.Impulse);
+            // Start coroutine for fade out.
+            StartCoroutine(FadeOutLight());
         }
+    }
+
+    IEnumerator FadeOutLight() {
+        // // Calculate the rate at which the intensity will decrease per second
+        // float fadeRate = initialIntensity / fadeDuration;
+        // // Set bumper's light to initial intensity
+        // bumperLight.intensity = initialIntensity;
+        // // Gradually decrease the intensity until it reaches zero
+        // while (bumperLight.intensity > 0f) {
+        //     bumperLight.intensity -= (float)Math.Sqrt(fadeRate * Time.deltaTime);
+        //     yield return null;
+        // }
+        // // Make sure the intensity reaches zero after the fade out
+        // bumperLight.intensity = 0f;
+    
+        float elapsedTime = 0f;
+
+        // Gradually decrease the intensity using the ease-out cubic function
+        while (elapsedTime < fadeDuration)
+        {
+            float t = elapsedTime / fadeDuration;
+            float intensity = initialIntensity - initialIntensity * EaseOutCubic(t);
+            bumperLight.intensity = intensity;
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        // Make sure the intensity reaches zero after the fade out
+        bumperLight.intensity = 0f;
+    }
+
+    // Ease-out cubic function
+    float EaseOutCubic(float t)
+    {
+        t--;
+        return t * t * t + 1;
     }
 }
