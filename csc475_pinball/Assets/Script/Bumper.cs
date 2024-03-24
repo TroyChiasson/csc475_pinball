b3a4b873dcf1dc52510463fe4c21c6aa28985ecf
ctm042 <ctm042@latech.edu>
2024-03-23 19:14:03 -0500

    Added bumper script. Rearranged gumdrop bumpers. Added tag to ball.

--- csc475_pinball/Assets/Script/Bumper.cs
+++ csc475_pinball/Assets/Script/Bumper.cs
@@ -2,17 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bumper : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class Bumper : MonoBehaviour {
+    public float force;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void OnCollisionEnter(Collision col) {
+        Debug.Log("Collision!");
+        if(col.gameObject.tag == "Ball") {
+            Debug.Log("With Ball!");
+            // Get the rigid body of the collided ball
+            Rigidbody ballrb = col.gameObject.GetComponent<Rigidbody>();
+            // Calculate angle between the collision point and the ball
+            Vector3 dir = col.contacts[0].point - transform.position;
+            // Get opposite angle (-Vector3) and normalize it
+            dir = -dir.normalized;
+            // Add force in the direction of dir and multiply it by force
+            ballrb.AddForce(dir * force, ForceMode.Impulse);
+        }
     }
 }
